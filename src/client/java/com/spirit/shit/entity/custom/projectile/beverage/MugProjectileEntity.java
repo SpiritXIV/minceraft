c19ae76098df252ef44884cb6870c6662a9d2f2e
KingZhara <WarOperationPlannedResponse@protonmail.com>
2023-09-29 20:06:12 -0500

    Provide example of custom damage types

--- src/client/java/com/spirit/shit/entity/custom/projectile/beverage/MugProjectileEntity.java
+++ src/client/java/com/spirit/shit/entity/custom/projectile/beverage/MugProjectileEntity.java
@@ -2,6 +2,7 @@ package com.spirit.shit.entity.custom.projectile.beverage;
 
 import com.spirit.shit.ShitMod;
 import com.spirit.shit.common.BeverageProjectileEntity;
+import com.spirit.shit.entity.damage.DamageTypes;
 import com.spirit.shit.item.ShitItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -12,15 +13,15 @@ import net.minecraft.world.World;
 
 public class MugProjectileEntity extends BeverageProjectileEntity {
     public MugProjectileEntity(EntityType<? extends ThrownItemEntity> entityType, World world) {
-        super(entityType, world, SoundEvents.BLOCK_GLASS_BREAK, "got_mugged");
+        super(entityType, world, SoundEvents.BLOCK_GLASS_BREAK, DamageTypes.of(world, DamageTypes.MUGGED));
     }
 
     public MugProjectileEntity(World world, LivingEntity owner) {
-        super(ShitMod.MugProjectileEntityType, world, owner, SoundEvents.BLOCK_GLASS_BREAK, "got_mugged");
+        super(ShitMod.MugProjectileEntityType, world, owner, SoundEvents.BLOCK_GLASS_BREAK, DamageTypes.of(world, DamageTypes.MUGGED));
     }
 
     public MugProjectileEntity(World world, double x, double y, double z) {
-        super(ShitMod.MugProjectileEntityType, world, x, y, z, SoundEvents.BLOCK_GLASS_BREAK, "got_mugged");
+        super(ShitMod.MugProjectileEntityType, world, x, y, z, SoundEvents.BLOCK_GLASS_BREAK, DamageTypes.of(world, DamageTypes.MUGGED));
     }
 
     @Override
