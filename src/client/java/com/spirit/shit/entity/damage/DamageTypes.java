e5c2f59ad6464dc1fdbcdd6bba7cff515102734e
KingZhara <WarOperationPlannedResponse@protonmail.com>
2023-09-29 20:05:55 -0500

    Provide framework for custom damage types

---
+++ src/client/java/com/spirit/shit/entity/damage/DamageTypes.java
@@ -0,0 +1,17 @@
+package com.spirit.shit.entity.damage;
+
+import com.mojang.serialization.Lifecycle;
+import com.spirit.shit.ShitMod;
+import net.minecraft.entity.damage.*;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+
+public interface DamageTypes {
+    RegistryKey<DamageType> MUGGED = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier("shit", "got_mugged"));
+
+    static DamageSource of(World world, RegistryKey<DamageType> key) {
+        return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
+    }
+}
