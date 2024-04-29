3eed075a66da42a449f1c68b54ff2f7fd302f6da
Spirit XIV <5p1r17574r0014@gmail.com>
2024-04-28 23:40:27 -0500

    fixed textures

--- src/main/java/com/spirit/shit/data/datagen/ShitLootTableProvider.java
+++ src/main/java/com/spirit/shit/data/datagen/ShitLootTableProvider.java
@@ -1,7 +1,5 @@
 package com.spirit.shit.data.datagen;
 
-import com.spirit.shit.global.block.ShitBlocks;
-import com.spirit.shit.global.item.ShitItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.minecraft.block.Block;
@@ -22,91 +20,6 @@ public class ShitLootTableProvider extends FabricBlockLootTableProvider {
 
     @Override
     public void generate() {
-        addDrop(ShitBlocks.CRACKLIN_BOX);
-        addDrop(ShitBlocks.GAS_CAN);
-        addDrop(ShitBlocks.GAS_CANISTER);
-        addDrop(ShitBlocks.TOILET);
-        addDrop(ShitBlocks.WORLD_BOMB);
-        addDrop(ShitBlocks.MODEM);
-        addDrop(ShitBlocks.COMPUTER_MOUSE);
-        addDrop(ShitBlocks.OLD_COMPUTER);
-        addDrop(ShitBlocks.RADIO);
-
-        //POSTER
-        addDrop(ShitBlocks.ULTRAKILL_POSTER);
-        
-        
-        // BACKROOMS LEVEL 0
-        addDrop(ShitBlocks.BACKROOMS_LIGHT);
-        addDrop(ShitBlocks.BACKROOMS_LIGHT_OFF);
-        addDrop(ShitBlocks.BACKROOMS_FLOOR);
-        addDrop(ShitBlocks.BACKROOMS_FLOOR_TELEPORT);
-        addDrop(ShitBlocks.BACKROOMS_WALLPAPER);
-        addDrop(ShitBlocks.BACKROOMS_WALLPAPER_LIP);
-        addDrop(ShitBlocks.BACKROOMS_CEILING);
-        addDrop(ShitBlocks.BACKROOMS_CEILING_VENT);
-
-        // BACKROOMS LEVEL 1
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_FLOOR);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_WALLS);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_WALLS_TUBES);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_WALL_PILLAR);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_WALL_PILLAR_LIGHT_TOP);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_WALL_PILLAR_LIGHT_BOTTOM);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_WALL_PILLAR_LIGHT_TOP_OFF);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_WALL_PILLAR_LIGHT_BOTTOM_OFF);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_WALLS_PAINTED);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_CEILING);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_CEILING_PAINTED);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_CEILING_PAINTED_LIGHT);
-        addDrop(ShitBlocks.BACKROOMS_CONCRETE_CEILING_PAINTED_LIGHT_OFF);
-
-        // BACKROOMS LEVEL 2
-        addDrop(ShitBlocks.BACKROOMS_PIPE_LARGE);
-        addDrop(ShitBlocks.BACKROOMS_PIPE_SMALL);
-        addDrop(ShitBlocks.BACKROOMS_WETTED_CONCRETE_FLOOR);
-        addDrop(ShitBlocks.BACKROOMS_WETTED_CONCRETE_WALL);
-        addDrop(ShitBlocks.BACKROOMS_WETTED_CONCRETE_CEILING);
-        addDrop(ShitBlocks.BACKROOMS_INDUSTRIAL_LIGHT);
-        addDrop(ShitBlocks.BACKROOMS_INDUSTRIAL_LIGHT_SHATTERED);
-
-        //PLUSHES
-        addDrop(ShitBlocks.ALPHA_PLUSH);
-        addDrop(ShitBlocks.CATLOVE_PLUSH);
-        addDrop(ShitBlocks.CHEFINSANITY_PLUSH);
-        addDrop(ShitBlocks.COMPUTER_PLUSH);
-        addDrop(ShitBlocks.ERIS_PLUSH);
-        addDrop(ShitBlocks.JARGEDES_PLUSH);
-        addDrop(ShitBlocks.JBIRD_PLUSH);
-        addDrop(ShitBlocks.KINGZHARA_PLUSH);
-        addDrop(ShitBlocks.MCFELLA_PLUSH);
-        addDrop(ShitBlocks.MILKYFUR_PLUSH);
-        addDrop(ShitBlocks.SIERRA_PLUSH);
-        addDrop(ShitBlocks.SLAZER_PLUSH);
-        addDrop(ShitBlocks.SPIRIT_PLUSH);
-
-        //WARZONE
-        addDrop(ShitBlocks.LAND_MINE);
-
-        // OTHER BLOCKS
-        addDrop(ShitBlocks.CUBE_BLUE);
-        addDrop(ShitBlocks.CUBE_GRAY);
-        addDrop(ShitBlocks.CUBE_GREEN);
-        addDrop(ShitBlocks.CUBE_LIGHTBLUE);
-        addDrop(ShitBlocks.CUBE_ORANGE);
-        addDrop(ShitBlocks.CUBE_PEACH);
-        addDrop(ShitBlocks.CUBE_PINK);
-        addDrop(ShitBlocks.CUBE_PURPLE);
-        addDrop(ShitBlocks.CUBE_RED);
-        addDrop(ShitBlocks.CUBE_RUSTY);
-        addDrop(ShitBlocks.CUBE_WHITE);
-        addDrop(ShitBlocks.CUBE_YELLOW);
-
-
-
-        //ORES
-        addDrop(ShitBlocks.BAUXITE_ORE, enchantedLikeOreDrops(ShitBlocks.BAUXITE_ORE, ShitItems.RAW_BAUXITE));
-        addDrop(ShitBlocks.DEEPSLATE_BAUXITE_ORE, enchantedLikeOreDrops(ShitBlocks.DEEPSLATE_BAUXITE_ORE, ShitItems.RAW_BAUXITE));
 
     }
         public LootTable.Builder enchantedLikeOreDrops(Block drop, Item item) {
--- src/main/java/com/spirit/shit/global/item/custom/ChipsBagItem.java
+++ src/main/java/com/spirit/shit/global/item/custom/ChipsBagItem.java
@@ -1,36 +1,44 @@
 package com.spirit.shit.global.item.custom;
 
+import com.spirit.shit.global.item.ShitItems;
 import com.spirit.shit.global.sound.ShitSounds;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsage;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
 public class ChipsBagItem extends Item {
 
+    private static final int MAX_USES = 9;
+    private int remainingUses;
+
     public ChipsBagItem(Settings settings) {
         super(settings);
+        this.remainingUses = MAX_USES;
     }
 
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        super.finishUsing(stack, world, user);
         if (user instanceof PlayerEntity && !((PlayerEntity) user).getAbilities().creativeMode) {
-            stack.decrement(1);
+            this.remainingUses--;
+            if (this.remainingUses <= 0) {
+                return new ItemStack(Items.AIR);
+            }
         }
         return stack;
     }
 
     @Override
     public int getMaxUseTime(ItemStack stack) {
-        return 3;
+        return 32;
     }
 
     @Override
@@ -50,7 +58,19 @@ public class ChipsBagItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        return ItemUsage.consumeHeldItem(world, user, hand);
-
+        if (!world.isClient) {
+            ItemStack heldItemStack = user.getStackInHand(hand);
+            if (heldItemStack.getItem() instanceof ChipsBagItem) {
+                ChipsBagItem item = (ChipsBagItem) heldItemStack.getItem();
+                int chipsToGive = (int) Math.ceil((double) item.remainingUses / MAX_USES * heldItemStack.getMaxCount());
+                item.remainingUses = 0;
+                ItemStack emptyBagStack = new ItemStack(Items.AIR);
+                if (!user.getInventory().insertStack(new ItemStack(ShitItems.PLAIN_CHIPS, chipsToGive))) {
+                    ItemScatterer.spawn(world, user.getBlockPos().getX(), user.getBlockPos().getY(), user.getBlockPos().getZ(), new ItemStack(ShitItems.PLAIN_CHIPS, chipsToGive)); // Replace with your chips item
+                }
+                user.setStackInHand(hand, emptyBagStack);
+            }
+        }
+        return TypedActionResult.success(user.getStackInHand(hand));
     }
 }
